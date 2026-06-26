154f45b003e8dbefcad45c1ba03b9b87b11b8116
GyuminLeo <gyuminleo@gmail.com>
2026-06-26 15:24:18 +0900

    Synced to latest state
    Change-Id: I5e4feefb2f12ef1eab827c3c2bc06097768ed1fa

--- sm7250-common-vendor.mk
+++ sm7250-common-vendor.mk
@@ -561,7 +561,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.sensorscalibrate@1.0-service \
     vendor.qti.hardware.soter@1.0-service \
     vendor.qti.hardware.tui_comm@1.0-service-qti \
-    immvibed \
     ims_rtp_daemon \
     imsdatadaemon \
     imsqmidaemon \
